9e05c08c8a87f54bf6eb1161f57b2ed09c8b8519
superhcheng <chenghongyu628@gmail.com>
2017-09-01 10:55:42 -0400

    add springmvc-struts with springweb and springmvc integration

---
+++ spring-mvc-struts2/src/main/java/us/supercheng/spring4/spring/springmvc/struts2/dao/IProductDao.java
@@ -0,0 +1,7 @@
+package us.supercheng.spring4.spring.springmvc.struts2.dao;
+
+import us.supercheng.spring4.spring.springmvc.struts2.entity.Product;
+
+public interface IProductDao {
+    void checkProduct(Product product);
+}
---
+++ spring-mvc-struts2/src/main/java/us/supercheng/spring4/spring/springmvc/struts2/struts/action/ProductAction.java
@@ -0,0 +1,8 @@
+package us.supercheng.spring4.spring.springmvc.struts2.struts.action;
+
+public class ProductAction {
+    public String execute(){
+        // BRs
+        return "product";
+    }
+}
