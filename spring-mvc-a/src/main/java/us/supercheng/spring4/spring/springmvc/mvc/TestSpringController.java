6797f3112f02e8c0208bf0e6546a704807b34a56
supercheng <chenghongyu628@gmail.com>
2017-09-28 11:50:08 -0400

    add Spring MVC RequestMapping with HTTP RequestHeaders, Request Params, HTTP Methods Options,

--- spring-mvc-a/src/main/java/us/supercheng/spring4/spring/springmvc/mvc/TestSpringController.java
+++ spring-mvc-a/src/main/java/us/supercheng/spring4/spring/springmvc/mvc/TestSpringController.java
@@ -2,17 +2,37 @@ package us.supercheng.spring4.spring.springmvc.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by supercheng on 9/25/17.
  */
 
 @Controller
+@RequestMapping("/apis")
 public class TestSpringController {
 
-    @RequestMapping("serverStatus")
+    @RequestMapping("/serverStatus")
     public String checkServerStatus(){
-        System.out.println("Server is Up & Running");
+        System.out.println("GET Method: Server is Up & Running");
+        return "success";
+    }
+
+    @RequestMapping(value = "/serverStatusPost", method = RequestMethod.POST)
+    public String checkServerStatusPost(){
+        System.out.println("POST Method: Server is Up & Running");
+        return "success";
+    }
+
+    @RequestMapping(value = "/serverSampleHeader", headers = "Host=localhost:8080", method = RequestMethod.GET)
+    public String checkServerSampleHeaderMethod(){
+        System.out.println("POST Method: Server is Up & Running");
+        return "success";
+    }
+
+    @RequestMapping(value = "/serverSampleParams", params = {"id=1", "name=test"}, method = RequestMethod.GET)
+    public String checkServerSampleParamsMethod(){
+        System.out.println("POST Method: Server is Up & Running");
         return "success";
     }
 }
--- spring-mvc-a/src/main/webapp/index.jsp
+++ spring-mvc-a/src/main/webapp/index.jsp
@@ -1,6 +1,27 @@
 <html>
 <body>
-    <h2>Hello World!</h2>
-    <a href="serverStatus">Go Success</a>
+    <h3>Server Status</h3>
+    <a href="apis/serverStatus">Go Success - GET</a>
+    <hr>
+
+    <form action="apis/serverStatusPost" method="post">
+        <input type="submit" value="Go Success - POST">
+    </form>
+    <hr>
+
+    <a href="apis/serverSampleHeader">Go Test Header RequestMapping - GET</a>
+    <hr>
+    <form action="apis/serverSampleHeader" method="post">
+        <input type="submit" value="Go Test Header RequestMapping - POST">
+    </form>
+    <hr>
+
+    <a href="apis/serverSampleParams">Go Test Params RequestMapping - GET</a>
+    <hr>
+    <form action="apis/serverSampleParams" method="post">
+        <input type="submit" value="Go Test Params RequestMapping - POST">
+    </form>
+    <hr>
+
 </body>
 </html>
